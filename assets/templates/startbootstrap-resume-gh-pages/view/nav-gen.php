281a335b26b6d099cf28682e39d4dc4353ea1abe
ch3wb4cc4 <44432818+ch3wb4cc4@users.noreply.github.com>
2022-11-24 15:09:07 +0100

    customized bootstrap template

---
+++ assets/templates/startbootstrap-resume-gh-pages/view/nav-gen.php
@@ -0,0 +1,15 @@
+<?php
+// can and should be made dynamic, of course...
+
+?>
+
+
+
+<li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo $GLOBALS['url_root']; ?>">HOme</a></li>
+<li class="nav-item"><a class="nav-link js-scroll-trigger" href="?module=login#about">Login</a></li>
+<li class="nav-item"><a class="nav-link js-scroll-trigger" href="?module=login&task=logout#about">Logout</a></li>
+<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+
+
+
